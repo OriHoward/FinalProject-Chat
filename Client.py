8246800370fe71107c256334556f239c7294ea7d
OriHoward <ori.howard@gmail.com>
2022-01-31 03:40:52 +0200

    client side small progress

--- Client.py
+++ Client.py
@@ -1,21 +1,36 @@
 import socket
 
+FORMAT = 'utf-8'
+HEADER = 64
+PORT = 55000
+IP = socket.gethostbyname(socket.gethostname())
+ADDR = (IP, PORT)
+
 
 class Client:
-    def __init__(self, name, _id):
+    def __init__(self, name: str = "", _id: str = ""):
         self.name = name
         self._id = _id
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        pass
+        self.client.connect(ADDR)
 
     def disconnect(self):
+        self.send_msg_to_server("DISCONNECT")
+
+    def send_msg_to_server(self, msg):
+        message = msg.encode(FORMAT)
+        msg_length =len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER-len(send_length)) # adding padding to fill the 64 bytes
+        self.client.send(send_length)
+        self.client.send(message)
+
+    def send_message_to(self, msg, client):
         pass
 
-    def send_message_to(self, client):
-        pass
-
-    def send_message_to_all(self):
+    def send_message_to_all(self, msg):
         pass
 
     def get_users(self):
@@ -29,5 +44,3 @@ class Client:
 
     def download_file(self):
         pass
-
-
--- Server.py
+++ Server.py
@@ -3,34 +3,35 @@ import threading
 
 from Client import Client
 
+
+FORMAT = 'utf-8'
 HEADER = 64
+PORT = 55000
+IP = socket.gethostbyname(socket.gethostname())
+ADDR = (IP, PORT)
 
 
 class Server:
     def __init__(self):
         self.clients: dict[Client] = {}
-        self.port: int = 55000
-        self.ip: str = socket.gethostbyname(socket.gethostname())
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.address: tuple = (self.ip, self.port)
-
-    def bind(self):
-        self.server.bind(self.address)
+        self.server.bind(ADDR)
 
     def handle_client(self, conn, addr):
-        print(f"[NEW CONNECTION] {addr} connected")
+        print(f"[NEW CONNECTION] {addr} connected.")
         connected = True
         while connected:
-            msg_length = conn.recv(HEADER).decode('utf-8')
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode('utf-8')
-            if msg == "DISCONNECT":
-                connected = False
-            print(f"[{addr}] {msg}")
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == "DISCONNECT":
+                    connected = False
+                print(f"[{addr}] {msg}")
         conn.close()
 
     def start(self):
-        print("[STARTING] server is starting..")
+        print("[LISTENING] server is now listening..")
         self.server.listen()
         while True:
             conn, addr = self.server.accept()
