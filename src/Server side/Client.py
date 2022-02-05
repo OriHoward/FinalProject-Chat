9782883929c28bbcadd14c76bdc2e37bc3d3673f
OriHoward <ori.howard@gmail.com>
2022-02-05 19:30:52 +0200

    GUI progress and added a queue with new messages for each client

--- src/Server side/Client.py
+++ src/Server side/Client.py
@@ -3,6 +3,7 @@ class Client:
         self.client_socket = client_socket
         self.user_name = user_name
         self.is_connected = True
+        self.new_msgs = []
 
     def __repr__(self):
         return f"user_name:{self.user_name}"
@@ -10,3 +11,7 @@ class Client:
     def disconnect(self):
         print(f"{self.user_name} has disconnected")
         self.is_connected = False
+
+    def get_new_msg(self):
+        if self.new_msgs:
+            return self.new_msgs.pop(0)
